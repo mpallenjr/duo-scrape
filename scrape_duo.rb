b17b627aad0d79199574adb229e06d184d5c8624
Mike Allen <mpallenjnr@gmail.com>
2024-10-10 12:40:58 -0500

    escaping speaking questions, getting network response

--- scrape_duo.rb
+++ scrape_duo.rb
@@ -1,7 +1,7 @@
 # scrape Duoliogo
 require "ferrum"
 require 'dotenv/load'
-browser = Ferrum::Browser.new(slowmo: 2)
+browser = Ferrum::Browser.new(slowmo: 3)
 page = browser.create_page
 page.go_to(ENV['DUOLINGO_URL'])
 email = page.at_xpath("//*[@id='web-ui1']")
@@ -12,4 +12,14 @@ page.at_xpath("//*[@class='WxjqG _1x5JY _1M9iF _36g4N _2YF0P _1QN-w']").click
 page.screenshot(path: "duo-1.png")
 page.go_to("https://www.duolingo.com/practice")
 page.screenshot(path: "duo-2.png")
+if page.at_xpath("//h1").text == "Select the matching pairs"
+  puts "true"
+  page.at_xpath("//*[@class='_1Qh5D _36g4N _2YF0P _2x7Co _3fo6Q']").click
+  page.screenshot(path: "duo-3.png")
+  page.at_xpath("//*[@class='_1x5JY _1M9iF _36g4N _2YF0P _3DbUj _1EqMR _1lyVV']").click
+  page.screenshot(path: "duo-4.png")
+else
+  puts page.at_xpath("//h1").text
+end
+puts page.network.response.body
 browser.quit
