a5914a4ad3bb351c0cf7b8f372ffbe6865490a0b
Mike Allen <mpallenjnr@gmail.com>
2024-10-10 10:59:18 -0500

    visiting page and entering creds

--- scrape_duo.rb
+++ scrape_duo.rb
@@ -2,6 +2,12 @@
 require "ferrum"
 
 browser = Ferrum::Browser.new
-browser.go_to("https://google.com")
-browser.screenshot(path: "google.png")
+page = browser.create_page
+page.go_to("https://www.duolingo.com/practice-hub")
+email = page.at_xpath("//*[@id='web-ui1']")
+email.focus.type("Hello")
+password = page.at_xpath("//*[@id='web-ui2']")
+password.focus.type("Hello")
+page.at_xpath("//*[@class='WxjqG _1x5JY _1M9iF _36g4N _2YF0P _1QN-w']").click
+page.screenshot(path: "duo.png")
 browser.quit
